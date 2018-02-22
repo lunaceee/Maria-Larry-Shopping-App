bd654b7402a10dce9ba11d8cce95f2728dbd9f6b
Larry Ioannidis <ilarion.ioannidis@gmail.com>
2018-02-22 12:42:35 +0000

    Added Logout To settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="settings">
+    <button v-on:click="logout">Logout</button>
   </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'hello',
+  methods: {
+    logout: function () {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.settings {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
